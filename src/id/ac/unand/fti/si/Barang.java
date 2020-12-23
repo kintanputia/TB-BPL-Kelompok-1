2a5af9af759bd5c0769c2cbc167204e1b75688af
Habib-20 <72001981+Habib-20@users.noreply.github.com>
2020-12-23 07:38:50 +0700

    Habib

---
+++ src/id/ac/unand/fti/si/Barang.java
@@ -0,0 +1,17 @@
+package id.ac.unand.fti.si;
+
+public class Barang {
+	
+	String sku, nama;
+	Integer stok, hbeli, hjual;
+	
+	public Barang (String Sku, String Nama, Integer Stok, Integer Hbeli, Integer Hjual) {
+		this.sku = Sku;
+		this.nama = Nama;
+		this.stok = Stok;
+		this.hbeli = Hbeli;
+		this.hjual = Hjual;
+	}
+	
+
+}
